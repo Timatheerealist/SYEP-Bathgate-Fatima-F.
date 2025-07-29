4b5577d2a59839d91a25cfb15c94eec7893f88b4
Timatheerealist <ffofanah2027@amsbronx.org>
2025-07-29 15:47:05 +0000

    Added JavaScript

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log("Hello world!");
+
+let myString = "a2b3c";
+console.log(myString);
+
+let myNumVar =123;
+console.log(typeof myNumVar);
+console.log(10 + 7 "eggs");
+
+
+//Both Conditions Need to be true 
+console.log (7>3)&&(5<10);
+
+//One conditions must be true
+console.log (10>3)||(10>12);
+
+
+//The statemnt is logical or not
+console.log(!5>3);
+
+
+function introduction(Fatima,16) {
+    console.log('Hello, My name is Fatima + name +"and i am + age + "years old")
+        introduction("Fatima ,16");
+
+}
+const hour = 1 new Date().getHours();
+let greeting = "";
+
+if (hour<12){
+    greeting = "Good Morning"
+}esle if (hour < 18){
+    gretting = "Good afternoon";
+} else {greeting = "Good evening"}
+
+
+
+
+document.getElementById ("greeting"). innerText = greetings;
+
+
+
